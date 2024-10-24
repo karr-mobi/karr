61c7b75d7e376165d5d33299bc4196b157d11643
finxol <git@finxol.io>
2024-10-24 20:15:14 +0200

    feat: add fastify-helmet

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import Fastify, { type FastifyInstance } from "fastify"
 import cookie from "fastify-cookie"
+import helmet from "@fastify/helmet"
 
 import logger from "./util/logger.ts"
 import sql from "./util/db.ts"
@@ -9,10 +10,18 @@ import { user } from "./routes/user.ts"
 import { account } from "./routes/account.ts"
 import { trip } from "./routes/trip.ts"
 
+/**
+ * Setup the Fastify app with all the routes and plugins
+ * @param opts Fastify options
+ * @returns Fastify app
+ */
 export const build = (opts = {}) => {
     const app: FastifyInstance = Fastify(opts)
 
     app.register(cookie)
+    app.register(helmet, {
+        global: true,
+    })
 
     app.register(system)
     app.register(user, { prefix: "/v1/user" })
