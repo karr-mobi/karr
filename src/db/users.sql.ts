66a5e75f62f79e521d292518d9ca0456c488da37
finxol <git@finxol.io>
2024-11-10 18:39:44 +0100

    feat(db): save special status title instead of id + separate first and last name

--- src/db/users.sql.ts
+++ src/db/users.sql.ts
@@ -4,10 +4,11 @@ import { specialStatusTable } from "./specialstatus.sql.ts"
 
 export const usersTable = pgTable("Users", {
     id: uuid().primaryKey().defaultRandom(),
-    name: text().notNull(),
+    firstName: text().notNull(),
+    lastName: text().notNull(),
     nickname: text(),
     phone: text().unique(),
     bio: text(),
     prefs: uuid().notNull().references(() => userPrefsTable.id),
-    specialStatus: uuid().references(() => specialStatusTable.id),
+    specialStatus: text().references(() => specialStatusTable.title),
 })
