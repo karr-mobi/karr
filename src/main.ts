612a843fc34f2f622d47dc837a1e65e24f630b7a
finxol <git@finxol.io>
2024-11-10 18:38:55 +0100

    feat: change Date to Temporal + add warning
    Log a warning if LOG_LEVEL is higher than info

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
 import { build } from "./server.ts"
 import logger from "./util/logger.ts"
-import { PORT } from "./util/config.ts"
+import { LOG_LEVEL, logLevels, PORT, PRODUCTION } from "./util/config.ts"
+
+if (!PRODUCTION && logLevels.findIndex((l) => l === LOG_LEVEL) < 2) {
+    logger.warn(
+        `Log level is set to '${LOG_LEVEL}' in a production environment.`,
+        "This may result in an excessive amount of logs.",
+        "/!\\ It may also log sensitive infornmation",
+        "Consider setting LOG_LEVEL to 'info' or 'warn' in .env",
+    )
+}
 
 logger.info("Starting server...")
-logger.info(`Timezone: ${Intl.DateTimeFormat().resolvedOptions().timeZone}`)
+logger.info(`Timezone: ${Temporal.Now.timeZoneId()}`)
 
 const app = build({
     logger: false,
