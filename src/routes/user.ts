b7f71ef7da4ddd1cbb161d29c9f10469ab29e980
finxol <git@finxol.io>
2024-11-11 00:13:10 +0100

    chore: write squeleton for all routes

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -4,46 +4,64 @@ import type {
     FastifyRequest,
     FastifyServerOptions,
 } from "fastify"
-import type { DataResponse, Response, UserWithPrefsAndStatus } from "../lib/types.d.ts"
+import type {
+    DataResponse,
+    ErrorResponse,
+    Response,
+    Users,
+    UserWithPrefsAndStatus,
+} from "../lib/types.d.ts"
 import logger from "../util/logger.ts"
 import { v4 as uuidv4 } from "@std/uuid"
-import { handleRequest, tmpResponse } from "../util/helpers.ts"
-import { selectUserById } from "../db/users.ts"
+import {
+    checkContentType,
+    handleRequest,
+    responseErrorObject,
+    tmpResponse,
+} from "../util/helpers.ts"
+import { selectUserById, updateNickname } from "../db/users.ts"
+
+// ==============================================
+// ========== Register endpoint routes ==========
+// ==============================================
 
 export const user = (
     fastify: FastifyInstance,
     _opts: FastifyServerOptions,
 ) => {
     fastify.get("/", getUser)
-    fastify.put("/nickname", tmpResponse) // TODO(@finxol)
-    fastify.put("/preferences", tmpResponse) // TODO(@finxol)
+    fastify.put("/nickname", {
+        preValidation: checkContentType,
+    }, changeNickname)
+    fastify.put("/preferences", {
+        preValidation: checkContentType,
+    }, updateUserPreferences)
     fastify.get("/trips", getUserTrips)
-    fastify.get("/bookings", tmpResponse) // TODO(@finxol)
-    fastify.get("/bookings/:id", tmpResponse) // TODO(@finxol)
-    fastify.delete("/bookings/:id", tmpResponse) // TODO(@finxol)
-    fastify.get("/:id", tmpResponse) // TODO(@finxol)
+    fastify.get("/bookings", getAllUserBookings)
+    fastify.get("/bookings/:id", getUserBooking)
+    fastify.delete("/bookings/:id", deleteUserBooking)
+    fastify.get("/:id", getUserPublicProfile)
 }
+
+// ==============================================
+// =========== Endpoint handler logic ===========
+// ==============================================
 
 /**
  * Get logged in user's info
  * @returns Object containing user info
  */
-const getUser = async (
+async function getUser(
     req: FastifyRequest,
     res: FastifyReply,
-): Promise<Response<object>> => {
+): Promise<Response<object>> {
     // get the user ID from the headers
-    const id: string = req.headers.Authorization || "c5976035-d774-49a0-ad03-81246ccbd00a" // default to local test user ID
-    logger.debug(`Getting user ${id}`)
+    const id: string = req.headers.authorization || ""
 
     // check the id is a valid UUID
     if (!uuidv4.validate(id)) {
         res.status(400)
-        return {
-            error: {
-                message: "Invalid user ID",
-            },
-        }
+        return <ErrorResponse> responseErrorObject("Invalid user ID")
     }
 
     // get the user from the database and send it back
@@ -51,14 +69,121 @@ const getUser = async (
 }
 
 /**
+ * Change the logged in user's nickname
+ * @returns {Response} Data response if update was successful, ErrorResponse if not
+ */
+async function changeNickname(
+    req: FastifyRequest,
+    res: FastifyReply,
+): Promise<Response<boolean>> {
+    const id: string = req.headers.authorization || ""
+    const nickname: string = req.body.nickname
+
+    // check the id is a valid UUID
+    if (!uuidv4.validate(id)) {
+        res.status(400)
+        return <ErrorResponse> responseErrorObject("Invalid user ID")
+    }
+
+    // check the nickname is a valid string
+    if (typeof nickname !== "string" || nickname.length < 1) {
+        return <ErrorResponse> responseErrorObject("Invalid nickname")
+    }
+
+    // update the user's nickname in the database
+    return await handleRequest<boolean>(res, () => updateNickname(id, nickname))
+}
+
+/**
+ * Update the logged in user's preferences
+ * @returns {Response} DataResponse if update was successful, ErrorResponse if not
+ */
+async function updateUserPreferences(
+    req: FastifyRequest,
+    res: FastifyReply,
+): Promise<Response<boolean>> {
+    // TODO(@finxol): Implement this
+    return await new Promise((resolve) => {
+        logger.debug("Updating user preferences")
+        resolve(tmpResponse(req, res))
+    })
+}
+
+/**
  * Get complete list of logged in user's trips
  * @returns {object} - Object containing list of trips
  */
-const getUserTrips = (_req: FastifyRequest, _res: FastifyReply): DataResponse<object> => {
+function getUserTrips(
+    _req: FastifyRequest,
+    _res: FastifyReply,
+): DataResponse<object> {
     logger.debug("Getting user trips")
     return {
         data: {
             trips: [],
         },
     }
+}
+
+/**
+ * Get all bookings for the logged in user
+ * @returns {Response} DataResponse if fetch was successful, ErrorResponse if not
+ */
+async function getAllUserBookings(
+    req: FastifyRequest,
+    res: FastifyReply,
+): Promise<Response<boolean>> {
+    // TODO(@finxol): Implement this
+    return await new Promise((resolve) => {
+        logger.debug("Getting all user bookings")
+        resolve(tmpResponse(req, res))
+    })
+}
+
+/**
+ * Get specific booking details. The logged in user must be concerned with the booking
+ * @returns {Response} DataResponse if fetch was successful, ErrorResponse if not
+ */
+async function getUserBooking(
+    req: FastifyRequest,
+    res: FastifyReply,
+): Promise<Response<boolean>> {
+    // TODO(@finxol): Implement this
+    // req.body should contain the booking ID
+    return await new Promise((resolve) => {
+        logger.debug("Getting user booking details")
+        resolve(tmpResponse(req, res))
+    })
+}
+
+/**
+ * Delete a specific booking. The logged in user must be concerned with the booking
+ * @returns {Response} DataResponse if deletion was successful, ErrorResponse if not
+ */
+async function deleteUserBooking(
+    req: FastifyRequest,
+    res: FastifyReply,
+): Promise<Response<boolean>> {
+    // TODO(@finxol): Implement this
+    // req.body should contain the booking ID
+    return await new Promise((resolve) => {
+        logger.debug("Deleting user booking")
+        resolve(tmpResponse(req, res))
+    })
+}
+
+/**
+ * Delete a specific booking. The logged in user must be concerned with the booking
+ * @returns {Response} DataResponse if fetch was successful, ErrorResponse if not
+ */
+async function getUserPublicProfile(
+    req: FastifyRequest,
+    res: FastifyReply,
+): Promise<Response<Users>> {
+    // TODO(@finxol): Implement this
+    // req.params should contain the user ID
+    return await new Promise((resolve) => {
+        logger.debug("Getting user public profile info")
+        resolve(tmpResponse(req, res))
+    })
 }
