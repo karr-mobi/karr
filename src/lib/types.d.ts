0eba84f7ab241c90bebc99f5720f376e4c33a7a9
finxol <git@finxol.io>
2024-11-10 15:52:05 +0100

    chore: create more types
    new types for Error responses and Response (either error or data) + all create db tables

--- src/lib/types.d.ts
+++ src/lib/types.d.ts
@@ -1,4 +1,42 @@
+import type { InferInsertModel, InferSelectModel } from "drizzle-orm"
+import type { usersTable } from "../db/users.sql.ts"
+import type { specialStatusTable } from "../db/specialstatus.sql.ts"
+import type { userPrefsTable } from "../db/userprefs.sql.ts"
+import type { accountsTable } from "../db/accounts.sql.ts"
+
 export interface DataResponse<T> {
     timestamp?: number
     data: T
 }
+
+export interface ErrorResponse {
+    timestamp?: number
+    error: {
+        message: string
+    }
+}
+
+export type Response<T> = DataResponse<T | object> | ErrorResponse
+
+export type SpecialStatus = InferSelectModel<typeof specialStatusTable>
+export type SpecialStatusInsert = Omit<InferInsertModel<typeof specialStatusTable>, "id">
+
+export type UserPrefs = InferSelectModel<typeof userPrefsTable>
+export type UserPrefsInsert = Omit<InferInsertModel<typeof userPrefsTable>, "id">
+
+export type Users = InferSelectModel<typeof usersTable>
+export type UsersInsert = Omit<InferInsertModel<typeof usersTable>, "id">
+
+export type UsersWithPrefs = {
+    Users: Users
+    UserPrefs: UserPrefs
+}
+
+export type UserWithPrefsAndStatus = {
+    Users: Users
+    UserPrefs: UserPrefs
+    SpecialStatus: SpecialStatus
+}
+
+export type Account = InferSelectModel<typeof accountsTable>
+export type AccountInsert = Omit<InferInsertModel<typeof accountsTable>, "id">
