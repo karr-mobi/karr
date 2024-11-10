0f13b05e31b0e40d4c2d5cf4cbd428831028c616
finxol <git@finxol.io>
2024-11-11 00:15:06 +0100

    chore: refactor
    - Move from Date to Temporal
    - Create responseErrorObject function to return template error message
    - Add checkContentType function

--- src/util/helpers.ts
+++ src/util/helpers.ts
@@ -1,11 +1,12 @@
 import type { DataResponse, ErrorResponse, Response } from "../lib/types.d.ts"
 import type { FastifyReply, FastifyRequest } from "fastify"
 import logger from "../util/logger.ts"
+import { ADMIN_EMAIL } from "./config.ts"
 
 /**
  * Template for a function that returns a response object
+ * @param _req The FastifyRequest object
  * @param res The FastifyReply object
- * @param fn The function to execute
  * @returns The response object
  */
 export function tmpResponse(
@@ -14,7 +15,7 @@ export function tmpResponse(
 ): Response<object> {
     res.status(418)
     return {
-        timestamp: Date.now(),
+        timestamp: Temporal.Now.instant().epochMilliseconds,
         data: {
             message: "I'm a teapot",
             img: "https://http.cat/images/418.jpg",
@@ -23,8 +24,30 @@ export function tmpResponse(
 }
 
 /**
+ * Helper function to create a response object
+ * @param message The error message
+ * @param args Any additional arguments
+ * @returns The error response object
+ */
+export function responseErrorObject(
+    message: string,
+    ...args: unknown[]
+): ErrorResponse {
+    return {
+        timestamp: Temporal.Now.instant().epochMilliseconds,
+        contact: ADMIN_EMAIL,
+        error: {
+            message: message,
+            details: args,
+        },
+    }
+}
+
+/**
  * Helper function which accepts any function
  * and wraps it in a try-catch block
+ * Appropriately handles the response object and status code
+ * @param res The FastifyReply object
  * @param fn The function to wrap
  * @returns The result of the function or an error object
  */
@@ -36,24 +59,35 @@ export async function handleRequest<T>(
         const out: T = await fn()
         if (!out) {
             res.status(404)
-            return <ErrorResponse> {
-                error: {
-                    message: "Not found",
-                },
-            }
+            return <ErrorResponse> responseErrorObject("Resource not found")
         }
         return <DataResponse<T>> {
-            timestamp: Date.now(),
+            timestamp: Temporal.Now.instant().epochMilliseconds,
             data: out,
         }
     } catch (err) {
         logger.error(err)
         res.status(500)
-        return <ErrorResponse> {
-            timestamp: Date.now(),
-            error: {
-                message: "An error occurred. Please try again later.",
-            },
-        }
+        return <ErrorResponse> responseErrorObject("Internal server error", err)
+    }
+}
+
+/**
+ * Middleware to check the content type of a request
+ * The content type must be application/json
+ * @param req The FastifyRequest object
+ * @param res The FastifyReply object
+ * @param done The callback to continue the request
+ */
+export function checkContentType(
+    req: FastifyRequest,
+    res: FastifyReply,
+    done: () => void,
+): void {
+    if (req.headers["content-type"] !== "application/json") {
+        res.status(400)
+            .send(responseErrorObject("Invalid content type. Must be application/json"))
+    } else {
+        done()
     }
 }
