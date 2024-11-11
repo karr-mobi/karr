976b38c69a3ce75fbc6befe75a01ea0c50fa10e0
finxol <git@finxol.io>
2024-11-11 19:03:32 +0100

    chore: add tsdoc + robots.txt route

--- src/routes/system.ts
+++ src/routes/system.ts
@@ -3,6 +3,38 @@ import { Hono } from "hono"
 
 const hono = new Hono()
 
+/**
+ * Get system information
+ */
+hono.get("/", (c) => {
+    logger.info("Getting system info")
+    return c.json({
+        name: "Fedicarpool",
+        version: "0.1.0",
+        description: "API for the federated carpooling system",
+    })
+})
+
+/**
+ * Disallow all bots from accessing the API
+ */
+hono.get("/robots.txt", (c) => {
+    logger.info("Getting system info")
+    return c.text("User-agent: *\nDisallow: /")
+})
+
+/**
+ * Get the available API versions
+ * @returns Object containing the best version and available versions
+ * @example
+ * ```
+ * GET /versions
+ * {
+ *   "bestVersion": "v1",
+ *   "availableVersions": ["v1"]
+ * }
+ * ```
+ */
 hono.get("/versions", (c) => {
     logger.info("Getting versions")
     return c.json({
@@ -11,6 +43,17 @@ hono.get("/versions", (c) => {
     })
 })
 
+/**
+ * Check the health of the system
+ * @returns Object containing the health status
+ * @example
+ * ```
+ * GET /health
+ * {
+ *   "dbInitialised": true,
+ *   "status": "ok"
+ * }
+ */
 hono.get("/health", (c) => {
     logger.info("Checking health")
     const dbInitialised = true
