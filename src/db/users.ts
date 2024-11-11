f61a4e938242afc5e605741a3a86492b21df7b98
finxol <git@finxol.io>
2024-11-11 20:00:36 +0100

    chore: add updateNickname & selectUserProfileById

--- src/db/users.ts
+++ src/db/users.ts
@@ -2,24 +2,52 @@ import drizzle from "../lib/db_conn.ts"
 import { usersTable } from "../db/users.sql.ts"
 import { eq } from "drizzle-orm"
 import { userPrefsTable } from "./userprefs.sql.ts"
-import type { UserWithPrefsAndStatus } from "../lib/types.d.ts"
+import type { UserPublicProfile, UserWithPrefsAndStatus } from "../lib/types.d.ts"
 import { specialStatusTable } from "./specialstatus.sql.ts"
-
-export const selectAllUsers = async () => {
-    return await drizzle.select().from(usersTable)
-}
+import logger from "../util/logger.ts"
 
 /**
  * Select a user by their ID
- * @param id The ID of the user to select. !! ID HAS TO BE UUID v4.
+ * @param id The ID of the user to select. Assumes uuid v4 format.
  * @returns The user with the given ID
  */
-export const selectUserById = async (id: string): Promise<UserWithPrefsAndStatus> => {
+export async function selectUserById(id: string): Promise<UserWithPrefsAndStatus> {
     const users = await drizzle
         .select()
         .from(usersTable)
         .where(eq(usersTable.id, id))
-        .leftJoin(userPrefsTable, eq(usersTable.prefs, userPrefsTable.id))
-        .leftJoin(specialStatusTable, eq(usersTable.specialStatus, specialStatusTable.id))
+        .leftJoin(
+            userPrefsTable,
+            eq(usersTable.prefs, userPrefsTable.id),
+        ).leftJoin(
+            specialStatusTable,
+            eq(usersTable.specialStatus, specialStatusTable.title),
+        ).limit(1)
     return <UserWithPrefsAndStatus> users[0]
+}
+
+export async function updateNickname(
+    id: string,
+    nickname: string,
+): Promise<boolean> {
+    logger.debug(`Updating nickname for user ${id} to ${nickname}`)
+    await drizzle
+        .update(usersTable)
+        .set({ nickname })
+        .where(eq(usersTable.id, id))
+    return Promise.resolve(true)
+}
+
+export async function selectUserProfileById(id: string): Promise<UserPublicProfile> {
+    const users = await drizzle
+        .select({
+            firstName: usersTable.firstName,
+            nickname: usersTable.nickname,
+            bio: usersTable.bio,
+            specialStatus: usersTable.specialStatus,
+        })
+        .from(usersTable)
+        .where(eq(usersTable.id, id))
+        .limit(1)
+    return <UserPublicProfile> users[0]
 }
