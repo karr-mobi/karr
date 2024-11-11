7b54ad735f5a438b4169e5636dfb4c63502e4b41
finxol <git@finxol.io>
2024-11-11 19:58:40 +0100

    feat: write email change & verified status check routes

---
+++ src/db/accounts.ts
@@ -0,0 +1,39 @@
+import drizzle from "../lib/db_conn.ts"
+import { accountsTable } from "./accounts.sql.ts"
+import { eq } from "drizzle-orm"
+import type { AccountVerified } from "../lib/types.d.ts"
+
+/**
+ * Change an account's email address
+ * @param id The ID of the account to update. Assumes uuid v4 format.
+ * @param email The new email address
+ * @returns Whether the update was successful
+ */
+export async function updateEmail(
+    id: string,
+    email: string,
+): Promise<boolean> {
+    await drizzle
+        .update(accountsTable)
+        .set({ email })
+        .where(eq(accountsTable.id, id))
+    return true
+}
+
+/**
+ * Check if a user is verified
+ * @param id The ID of the account to check. Assumes uuid v4 format.
+ * @returns Whether the user is verified
+ */
+export async function isVerified(id: string): Promise<AccountVerified> {
+    const accounts = await drizzle
+        .select({
+            verified: accountsTable.verified,
+        })
+        .from(accountsTable)
+        .where(eq(accountsTable.user, id))
+        .limit(1)
+    return {
+        verified: accounts[0]?.verified === true,
+    }
+}
--- src/routes/account.ts
+++ src/routes/account.ts
@@ -1,6 +1,9 @@
-import type { DataResponse } from "../lib/types.d.ts"
+import type { AccountVerified, DataResponse } from "../lib/types.d.ts"
 import logger from "../util/logger.ts"
 import { Hono } from "hono"
+import { v4 as uuidv4 } from "@std/uuid"
+import { isVerified, updateEmail } from "../db/accounts.ts"
+import { handleRequest, responseErrorObject } from "../util/helpers.ts"
 
 const hono = new Hono()
 
@@ -16,7 +19,48 @@ hono.get("/", (c) => {
     )
 })
 
-hono.get("/verified", (c) => c.text("TODO")) // TODO(@finxol)
+/**
+ * Change the logged in user's email address
+ * @returns DataResponse if update was successful, ErrorResponse if not
+ */
+hono.put("/email", async (c) => {
+    // get the user ID from the headers
+    const id: string = c.req.header("Authorization") || ""
+    const { email } = await c.req.json()
+
+    // check the id is a valid UUID
+    if (!uuidv4.validate(id)) {
+        c.status(400)
+        return responseErrorObject(c, "Invalid user ID")
+    }
+
+    // check the nickname is a valid string
+    if (typeof email !== "string" || email.length < 1) {
+        c.status(400)
+        return responseErrorObject(c, "Invalid nickname")
+    }
+
+    // Change the email in the database
+    return await handleRequest<boolean>(c, () => updateEmail(id, email))
+})
+
+/**
+ * Check if a user is verified
+ */
+hono.get("/verified", async (c) => {
+    // get the user ID from the headers
+    const id: string = c.req.header("Authorization") || ""
+
+    // check the id is a valid UUID
+    if (!uuidv4.validate(id)) {
+        c.status(400)
+        return responseErrorObject(c, "Invalid user ID")
+    }
+
+    // Check if the user is verified
+    return await handleRequest<AccountVerified>(c, () => isVerified(id))
+})
+
 hono.post("/verify", (c) => c.text("TODO")) // TODO(@finxol)
 hono.delete("/", (c) => c.text("TODO")) // TODO(@finxol)
 
