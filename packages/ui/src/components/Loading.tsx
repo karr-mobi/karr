45a59a1e4c0bf56920d6b3a45d1bf420847276b0
finxol <git@finxol.io>
2024-12-12 15:24:02 +0100

    fix: use React 19 + fix linting

---
+++ packages/ui/src/components/Loading.tsx
@@ -0,0 +1,14 @@
+import type { ComponentPropsWithoutRef } from "react"
+
+export default function Loading({
+    className = "",
+    ...props
+}: ComponentPropsWithoutRef<"div">) {
+    return (
+        <div
+            className={"animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-gray-200" +
+                className}
+            {...props}
+        />
+    )
+}
---
+++ packages/ui/src/components/index.ts
@@ -0,0 +1,2 @@
+export { default as Button } from "./Button.tsx"
+export { default as Loading } from "./Loading.tsx"
