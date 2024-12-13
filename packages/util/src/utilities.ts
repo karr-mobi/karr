290a390dd3abb14a5584d022b5a087c1087cdafa
finxol <git@finxol.io>
2024-12-14 00:53:35 +0100

    chore: add toInt

--- packages/util/src/utilities.ts
+++ packages/util/src/utilities.ts
@@ -13,3 +13,22 @@ export function toCamelCase(str: string): string {
         (match) => match[1].toUpperCase(),
     )
 }
+
+/**
+ * Convert a string or number to an integer
+ * @param value - The value to convert
+ * @returns The integer value or undefined if the value is not a number
+ * @throws If the value is not a number
+ */
+export function toInt(value: number | string): number {
+    if (typeof value === "number") {
+        return value
+    }
+    const parsed = parseInt(value, 10)
+    if (isNaN(parsed)) {
+        throw new Error(
+            `${value} is not a number \n \t\t\tHINT: likely an invalid environment variable`,
+        )
+    }
+    return parsed
+}
