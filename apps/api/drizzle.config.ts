5d116f86b976c0369766cddf4af7b51a743180c3
finxol <git@finxol.io>
2025-01-07 16:26:59 +0100

    fix: update config import

--- apps/api/drizzle.config.ts
+++ apps/api/drizzle.config.ts
@@ -1,12 +1,12 @@
 import { defineConfig } from "drizzle-kit"
 
-import { DB_CONFIG } from "@karr/config"
+import getAppConfig from "@karr/config"
 
 export default defineConfig({
     out: "./drizzle",
     schema: "./src/db/schemas/*.ts",
     dialect: "postgresql",
     dbCredentials: {
-        url: DB_CONFIG.connStr
+        url: getAppConfig().DB_CONFIG.connStr
     }
 })
