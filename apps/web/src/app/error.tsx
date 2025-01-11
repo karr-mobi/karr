2516c804563982453f2cd35f7e536e2ac266756c
finxol <git@finxol.io>
2025-01-11 22:12:10 +0100

    feat: add error page

---
+++ apps/web/src/app/error.tsx
@@ -0,0 +1,28 @@
+"use client"
+
+import { useEffect } from "react"
+
+import { Button } from "@karr/ui/button"
+
+export default function Error({
+    error,
+    reset
+}: {
+    error: Error & { digest?: string }
+    reset: () => void
+}) {
+    useEffect(() => {
+        // Log the error to an error reporting service
+        console.error(error)
+    }, [error])
+
+    return (
+        <div className="flex min-h-screen flex-col items-center justify-center px-4 text-center">
+            <p className="text-sm font-semibold text-indigo-600">Error {error.name}</p>
+            <p className="mt-2 text-sm text-gray-500">
+                Sorry, an unexpected error has occurred.
+            </p>
+            <Button onClick={reset}>Refresh</Button>
+        </div>
+    )
+}
