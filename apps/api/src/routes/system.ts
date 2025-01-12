91ec17f523abb634d087adef409330a4b8a185a0
finxol <git@finxol.io>
2025-01-12 22:11:47 +0100

    fix(api): return the app name from config

--- apps/api/src/routes/system.ts
+++ apps/api/src/routes/system.ts
@@ -1,5 +1,7 @@
 import { Hono } from "hono"
 
+import { getAppConfig } from "@karr/config"
+
 const hono = new Hono()
 
 /**
@@ -7,7 +9,7 @@ const hono = new Hono()
  */
 hono.get("/", (c) => {
     return c.json({
-        name: "Karr API",
+        name: `${getAppConfig().APPLICATION_NAME} API`,
         version: "0.1.0",
         description: "API for the federated carpooling system"
     })
