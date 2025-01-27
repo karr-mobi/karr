68d7ba54fb7163c177032a9311f74f382a3ecf47
finxol <git@finxol.io>
2025-01-27 14:41:30 +0100

    feat(web): redirect to auth when logged out

---
+++ apps/web/src/middleware.ts
@@ -0,0 +1,16 @@
+import { NextResponse } from "next/server"
+import type { NextRequest } from "next/server"
+
+export function middleware(request: NextRequest) {
+    const isAuthenticated = request.cookies.get("isAuthenticated")
+
+    if (!isAuthenticated && request.nextUrl.pathname.startsWith("/trips")) {
+        return NextResponse.redirect(new URL("/auth/login", request.url))
+    }
+
+    return NextResponse.next()
+}
+
+export const config = {
+    matcher: "/trips/:path*"
+}
