8ee715a588aebe9a684e1310b8e436ab01912c18
Saunier Martin <blasster35@gmail.com>
2025-01-29 22:50:49 +0100

    feat(web): implement locale validation and integrate NextIntlClientProvider in layout

--- apps/web/src/app/[locale]/layout.tsx
+++ apps/web/src/app/[locale]/layout.tsx
@@ -1,30 +1,30 @@
 import type { Metadata } from "next"
 import localFont from "next/font/local"
-import Image from "next/image"
-import Link from "next/link"
 
-import { Button } from "@karr/ui/components/button"
-import { Separator } from "@karr/ui/components/separator"
 import { Toaster } from "@karr/ui/components/sonner"
 
 import "@/assets/globals.css"
 
+import { notFound } from "next/navigation"
+/** next-intl imports */
+import { NextIntlClientProvider } from "next-intl"
+import { getMessages } from "next-intl/server"
+
 import { APPLICATION_NAME } from "@/util/appname"
-import logo from "@/assets/logo-tmp.jpg"
+import { routing } from "@/i18n/routing"
 
 import Footer from "../_components/footer"
-import LoginAccount from "../_components/loginaccount"
+import Header from "../_components/header"
 import ThemeProvider from "../_components/ThemeProvider"
-import ThemeSwitch from "../_components/ThemeSwitch"
 import { CSPostHogProvider } from "./providers"
 
 const geistSans = localFont({
-    src: "../assets/fonts/GeistVF.woff",
+    src: "../../assets/fonts/GeistVF.woff",
     variable: "--font-geist-sans"
 })
 
 const baskervville = localFont({
-    src: "../assets/fonts/BaskervvilleRegular.woff2",
+    src: "../../assets/fonts/BaskervvilleRegular.woff2",
     variable: "--font-baskervville",
     display: "swap"
 })
@@ -34,14 +34,26 @@ export const metadata: Metadata = {
     description: `${APPLICATION_NAME} is a federated carpool platform.`
 }
 
-export default function RootLayout({
-    children
+export default async function RootLayout({
+    children,
+    params
 }: Readonly<{
     children: React.ReactNode
+    params: Promise<{ locale: string }>
 }>) {
+    const { locale } = await params
+
+    // Ensure that the incoming `locale` is valid
+    if (!routing.locales.includes(locale as any)) {
+        notFound()
+    }
+
+    // Providing all messages to the client
+    // side is the easiest way to get started
+    const messages = await getMessages()
     return (
         <html
-            lang="en"
+            lang={locale}
             suppressHydrationWarning
             className={`${geistSans.variable} ${baskervville.variable}`}
         >
@@ -56,47 +68,16 @@ export default function RootLayout({
                         enableSystem
                         disableTransitionOnChange
                     >
-                        <div className="grid h-screen grid-cols-1 grid-rows-[auto_1fr]">
-                            <header className="bg-primary/4 flex h-16 w-full flex-row items-center justify-between px-4 py-2">
-                                <div className="flow-inline flex flex-row items-center justify-end">
-                                    <Link
-                                        href="/"
-                                        className="flex flex-row items-center justify-start gap-4"
-                                    >
-                                        <Image
-                                            alt="Karr"
-                                            src={logo}
-                                            width={40}
-                                            height={40}
-                                            className="rounded-lg"
-                                            placeholder="blur"
-                                        />
-                                        <h5>{APPLICATION_NAME}</h5>
-                                    </Link>
-                                    <Separator orientation="vertical" className="h-8" />
-                                    <nav className="flex flex-row items-center justify-end gap-4">
-                                        <Button
-                                            asChild
-                                            variant="link"
-                                            className="text-md px-0"
-                                        >
-                                            <Link href="/trips/search">Trips</Link>
-                                        </Button>
-                                    </nav>
-                                </div>
-                                <div className="flex flex-row items-center justify-end gap-4">
-                                    <nav className="flex flex-row items-center justify-end gap-4">
-                                        <LoginAccount />
-                                    </nav>
-                                    <ThemeSwitch />
-                                </div>
-                            </header>
-                            <main className="mx-4 mt-4 flex h-full flex-col items-center justify-start overflow-y-scroll">
-                                {children}
-                            </main>
-                            <Footer />
-                        </div>
-                        <Toaster richColors />
+                        <NextIntlClientProvider messages={messages}>
+                            <div className="grid h-screen grid-cols-1 grid-rows-[auto_1fr]">
+                                <Header />
+                                <main className="mx-4 mt-4 flex h-full flex-col items-center justify-start overflow-y-scroll">
+                                    {children}
+                                </main>
+                                <Footer />
+                            </div>
+                            <Toaster richColors />
+                        </NextIntlClientProvider>
                     </ThemeProvider>
                 </body>
             </CSPostHogProvider>
