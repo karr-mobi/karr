bc29b55702a95ca980c1831d62e96c4bc283486d
finxol <git@finxol.io>
2025-03-01 18:33:12 +0100

    feat(ui): add loading `Spinner` component

--- apps/web/src/app/[locale]/stepper-example/page.tsx
+++ apps/web/src/app/[locale]/stepper-example/page.tsx
@@ -1,27 +1,33 @@
 import { Stepper } from "@karr/ui/components/stepper"
 
+import Loading from "@/components/Loading"
+
 export default function StepperExample() {
     return (
-        <Stepper>
-            <div>Step 1</div>
-            <div>Step 2</div>
-            <div>
-                Super wide step
-                <br />
-                Steeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeooeeeeeeeeeeeeeeeeeeeeeeeep
-                <br />
-                Step 3
-            </div>
-            <div>Step 4</div>
-            <div>
-                Super tall step
-                <br />
-                <br />
-                Step 5
-            </div>
-            <div>Step 6</div>
-            <div>Step 7</div>
-            <div>Step 8</div>
-        </Stepper>
+        <div className="mt-12 ">
+            <Loading />
+
+            <Stepper className="mt-12">
+                <div>Step 1</div>
+                <div>Step 2</div>
+                <div>
+                    Super wide step
+                    <br />
+                    Steeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeooeeeeeeeeeeeeeeeeeeeeeeeep
+                    <br />
+                    Step 3
+                </div>
+                <div>Step 4</div>
+                <div>
+                    Super tall step
+                    <br />
+                    <br />
+                    Step 5
+                </div>
+                <div>Step 6</div>
+                <div>Step 7</div>
+                <div>Step 8</div>
+            </Stepper>
+        </div>
     )
 }
--- apps/web/src/components/Loading.tsx
+++ apps/web/src/components/Loading.tsx
@@ -1,7 +1,9 @@
+import { Spinner } from "@karr/ui/components/spinner"
+
 export default function Loading() {
     return (
         <div className="flex flex-row items-center justify-center gap-4">
-            <div className="h-8 w-8 animate-spin rounded-full border-b-2 border-t-2 border-gray-900 dark:border-gray-300"></div>
+            <Spinner variant="ring" />
             <p className="text-gray-500">Loading...</p>
         </div>
     )
