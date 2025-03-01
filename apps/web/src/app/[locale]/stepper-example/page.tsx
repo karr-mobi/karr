93b3eb276abe2b87c898a3a39fd63bc1ebdcc204
finxol <git@finxol.io>
2025-03-01 17:47:52 +0100

    feat(ui): add `Stepper` + `TextMorph`

---
+++ apps/web/src/app/[locale]/stepper-example/page.tsx
@@ -0,0 +1,27 @@
+import { Stepper } from "@karr/ui/components/stepper"
+
+export default function StepperExample() {
+    return (
+        <Stepper>
+            <div>Step 1</div>
+            <div>Step 2</div>
+            <div>
+                Super wide step
+                <br />
+                Steeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeooeeeeeeeeeeeeeeeeeeeeeeeep
+                <br />
+                Step 3
+            </div>
+            <div>Step 4</div>
+            <div>
+                Super tall step
+                <br />
+                <br />
+                Step 5
+            </div>
+            <div>Step 6</div>
+            <div>Step 7</div>
+            <div>Step 8</div>
+        </Stepper>
+    )
+}
