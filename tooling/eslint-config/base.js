c247d853c8ff32a62ad8a2dfac691c2a90284900
Colin Ozanne <git@finxol.io>
2025-03-28 17:16:16 +0000

    build: move to Biome (#91)
    Prettier is nice but kinda slow
    
    waiting for Oxc to be ready to move ESLint over

--- tooling/eslint-config/base.js
+++ tooling/eslint-config/base.js
@@ -1,7 +1,6 @@
 import js from "@eslint/js"
 import tsPlugin from "@typescript-eslint/eslint-plugin"
 import tsParser from "@typescript-eslint/parser"
-import eslintConfigPrettier from "eslint-config-prettier"
 import turboPlugin from "eslint-plugin-turbo"
 import jsoncParser from "jsonc-eslint-parser"
 import tseslint from "typescript-eslint"
@@ -15,7 +14,6 @@ import packageJsonPlugin from "./plugins/package-json-plugin.js"
  * */
 export const config = [
     js.configs.recommended,
-    eslintConfigPrettier,
     ...tseslint.configs.recommended,
     {
         files: ["**/*.ts", "**/*.tsx"],
