d7966158165186607eda2350b422a5ab52decca7
finxol <git@finxol.io>
2025-04-08 15:49:54 +0200

    feat: add marquee animation

--- apps/web/src/app/[locale]/stepper-example/page.tsx
+++ apps/web/src/app/[locale]/stepper-example/page.tsx
@@ -1,4 +1,5 @@
 import { Stepper } from "@karr/ui/components/stepper"
+import { Marquee } from "@karr/ui/components/marquee"
 
 import Loading from "@/components/Loading"
 
@@ -6,6 +7,24 @@ export default function StepperExample() {
     return (
         <div className="mt-12 ">
             <Loading />
+
+            <div className="flex flex-col gap-4 my-4">
+                <Marquee
+                    baseVelocity={-3}
+                    direction="left"
+                    className="bg-green-500 text-white py-2"
+                >
+                    Karr is very cool
+                </Marquee>
+
+                <Marquee
+                    baseVelocity={-3}
+                    direction="right"
+                    className="bg-purple-500 text-white py-2"
+                >
+                    It's made by finxol
+                </Marquee>
+            </div>
 
             <Stepper className="mt-12">
                 <div>Step 1</div>
