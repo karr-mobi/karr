c91107c3e22ca9c341c2e49bc87d309d315d06a0
finxol <git@finxol.io>
2025-04-08 15:26:02 +0200

    feat: remove auth check from middleware
    Auth now needs to be checked directly in pages that require auth

--- apps/web/src/middleware.ts
+++ apps/web/src/middleware.ts
@@ -1,4 +1,3 @@
-import { NextResponse } from "next/server"
 import type { NextRequest } from "next/server"
 import createMiddleware from "next-intl/middleware"
 
@@ -7,30 +6,7 @@ import { routing } from "./i18n/routing"
 const i18nMiddleware = createMiddleware(routing)
 
 export function middleware(request: NextRequest) {
-    const response = i18nMiddleware(request)
-
-    if (response && !response.ok) {
-        // response not in the range 200-299 (usually a redirect)
-        // no need to execute the auth middleware
-        return response
-    }
-
-    return authMiddleware(request, response)
-}
-
-function authMiddleware(request: NextRequest, response: NextResponse) {
-    const isAuthenticated = request.cookies.get("auth-token") !== undefined
-    const localesPattern = routing.locales.join("|")
-    const regex = new RegExp(`^/(${localesPattern})/trips/`)
-
-    const locale = request.nextUrl.pathname.split("/")[1]
-
-    // this regex needs updating when adding a language
-    if (!isAuthenticated && request.nextUrl.pathname.match(regex)) {
-        return NextResponse.redirect(new URL(`/${locale}/auth/login`, request.url))
-    }
-
-    return response
+    return i18nMiddleware(request)
 }
 
 export const config = {
