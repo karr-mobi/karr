9d2c62a94e6126dd283c781351029016ab14e737
finxol <git@finxol.io>
2025-04-29 15:15:01 +0200

    feat: add `taze` for deps upgrade
    https://github.com/antfu-collective/taze

---
+++ taze.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from "taze"
+
+export default defineConfig({
+    // ignore packages from bumping
+    exclude: [],
+    // fetch latest package info from registry without cache
+    force: false,
+    // write to package.json
+    write: true,
+    // run `pnpm install` right after bumping
+    install: true,
+    // ignore paths for looking for package.json in monorepo
+    ignorePaths: ["**/node_modules/**"],
+    // ignore package.json that in other workspaces (with their own .git,pnpm-workspace.yaml,etc.)
+    ignoreOtherWorkspaces: true,
+    // override with different bumping mode for each package
+    packageMode: {}
+})
