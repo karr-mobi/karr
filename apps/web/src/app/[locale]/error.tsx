a0dae30b52e1af663c8f14bec113344fd77eda3f
finxol <git@finxol.io>
2025-05-01 19:52:41 +0200

    feat: add error page translations

--- apps/web/src/app/[locale]/error.tsx
+++ apps/web/src/app/[locale]/error.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import { useEffect } from "react"
+import { useTranslations } from "next-intl"
 
 import { Button } from "@karr/ui/components/button"
 
@@ -11,19 +12,19 @@ export default function Error({
     error: Error & { digest?: string }
     reset: () => void
 }) {
+    const t = useTranslations("Errors.500")
+
     useEffect(() => {
         // Log the error to an error reporting service
         console.error(error)
     }, [error])
 
     return (
-        <div className="flex min-h-screen flex-col items-center justify-center px-4 text-center">
+        <div className="w-full flex min-h-screen flex-col items-center justify-center px-4 text-center">
             <p className="text-sm font-semibold text-indigo-600">
-                Error {error.name}
+                {t("title", { name: error.name })}
             </p>
-            <p className="mt-2 text-sm">
-                Sorry, an unexpected error has occurred.
-            </p>
+            <p className="mt-2 text-sm">{t("message")}</p>
             <Button onClick={reset}>Refresh</Button>
         </div>
     )
