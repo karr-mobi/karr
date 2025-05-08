a09475af6e8ee2593d7258b69c2bad2b875af855
finxol <git@finxol.io>
2025-05-08 02:06:53 +0200

    fix: center loading indicator boundary

--- apps/web/src/app/[locale]/loading.tsx
+++ apps/web/src/app/[locale]/loading.tsx
@@ -1,5 +1,9 @@
 import { Loading as LoadingIndicator } from "@/components/Loading"
 
 export default function Loading() {
-    return <LoadingIndicator />
+    return (
+        <div className="w-full flex flex-1 flex-col items-center justify-center px-4 text-center">
+            <LoadingIndicator />
+        </div>
+    )
 }
