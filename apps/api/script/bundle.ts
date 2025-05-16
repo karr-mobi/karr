509b161941fa98868a8cc0d86fe111889f3f9d25
Colin Ozanne <git@colinozanne.fr>
2025-05-16 17:40:14 +0200

    ci: fix script ordering

--- apps/api/script/bundle.ts
+++ apps/api/script/bundle.ts
@@ -2,6 +2,7 @@
 
 import process from "node:process"
 import { c, logger } from "@karr/logger"
+import { tryCatch } from "@karr/util"
 import { build } from "esbuild"
 import { runtime } from "std-env"
 //@ts-expect-error we need to use a .ts here for it to be imported by node without compilation
@@ -11,39 +12,41 @@ logger.info("Bundling API...")
 
 const start = Date.now()
 
-build({
-    entryPoints: ["src/main.ts"],
-    bundle: true,
-    minify: true,
-    platform: "node",
-    target: "esnext",
-    format: "esm",
-    outdir: "out",
-    metafile: true,
-    external: ["node:*", "bun:*"],
-    plugins: [fixNodeBuiltins()],
-    splitting: true,
-    outExtension: { ".js": ".mjs" },
-    conditions: [runtime]
-})
-    .catch((err) => {
-        logger.error("Build failed:", err)
-        process.exit(1)
+const result = await tryCatch(
+    build({
+        entryPoints: ["src/main.ts"],
+        bundle: true,
+        minify: true,
+        platform: "node",
+        target: "esnext",
+        format: "esm",
+        outdir: "out",
+        metafile: true,
+        external: ["node:*", "bun:*"],
+        plugins: [fixNodeBuiltins()],
+        splitting: true,
+        outExtension: { ".js": ".mjs" },
+        conditions: [runtime]
     })
-    .then((result) => {
-        const end = Date.now()
-        const duration = end - start
-        const bytes = Object.keys(result.metafile.outputs)
-            .map((name) => result.metafile.outputs[name]?.bytes)
-            .reduce((acc, size) => acc! + size!, 0)
+)
 
-        logger.debug(
-            `Generated ${Object.keys(result.metafile.outputs).length} chunks`
-        )
+if (!result.success) {
+    logger.error("Build failed:", result.error)
+    process.exit(1)
+}
 
-        logger.success(
-            `⚡ Bundled API in ${duration}ms ${c.bold(
-                `(${(bytes! / 1024).toFixed(2)} kB)`
-            )}`
-        )
-    })
+const end = Date.now()
+const duration = end - start
+const bytes = Object.keys(result.value.metafile.outputs)
+    .map((name) => result.value.metafile.outputs[name]?.bytes)
+    .reduce((acc, size) => acc! + size!, 0)
+
+logger.debug(
+    `Generated ${Object.keys(result.value.metafile.outputs).length} chunks`
+)
+
+logger.success(
+    `⚡ Bundled API in ${duration}ms ${c.bold(
+        `(${(bytes! / 1024).toFixed(2)} kB)`
+    )}`
+)
