bbc3d136b29405a0572abe7752a6c8e085410754
Colin Ozanne <git@colinozanne.fr>
2025-07-13 18:08:58 +0100

    chore: add doc

--- apps/api/src/util/subject.ts
+++ apps/api/src/util/subject.ts
@@ -2,6 +2,10 @@ import type { UserSubject } from "@karr/auth/subjects"
 import logger from "@karr/logger"
 import type { Context } from "hono"
 
+/**
+ * Get the user subject from the Hono context.
+ * Auth is checked before setting the subject in the context.
+ */
 export function getUserSub(c: Context) {
     // Get the subject from the context
     const subject = c.get("userSubject") as UserSubject | undefined
