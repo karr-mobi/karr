581964fc0d27acbcb46083817deb28c5a2b2cb74
Colin Ozanne <git@colinozanne.fr>
2025-08-05 17:04:14 +0200

    fix(config): properly load config values at runtime

--- apps/web/src/util/apifetch.ts
+++ apps/web/src/util/apifetch.ts
@@ -1,11 +1,7 @@
+"use server"
+
+import { API_BASE, APP_URL } from "@karr/config"
 import { ofetch } from "ofetch"
-
-//biome-ignore lint: I need
-const API_BASE = process.env.NEXT_PUBLIC_API_BASE as string
-//biome-ignore lint: I need
-const APP_URL = process.env.NEXT_PUBLIC_APP_URL as string
-
-console.log("API_BASE", API_BASE)
 
 export const apiFetch = ofetch.create({
     baseURL: new URL(API_BASE, APP_URL).toString(),
